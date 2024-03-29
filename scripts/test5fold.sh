d99b8a711b5baa8a74e0b0acdb65f62dfe70574e
Mayuna Gupta <mayunag.cstaff@cse.iitd.ac.in>
2024-03-29 12:32:57 +0530

    Updated testing script for GBC

--- scripts/test5fold.sh
+++ scripts/test5fold.sh
@@ -20,6 +20,7 @@ GPUS_PER_NODE=${GPUS_PER_NODE:-2}  # Number of GPUs in each node
 SRUN_ARGS=${SRUN_ARGS:-""}  # Other slurm task args
 PY_ARGS=${@:3} 
 NODE_RANK=0
+INPUT_SIZE=224
 
 
 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per_node=${GPUS_PER_NODE} \
@@ -33,21 +34,13 @@ torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         --batch_size 2 \
-        --input_size 224 \
-        --short_side_size 224 \
-        --num_frames 16 \
+        --input_size ${INPUT_SIZE} \
         --num_workers 10 \
         --sampling_rate 3 \
         --num_sample 4 \
         --num_workers 4 \
-        --opt adam \
+        --opt adamw \
         --num_segment 1 \
-        --lr 7e-4 \
-        --min_lr 1e-5 \
-        --opt_betas 0.9 0.999 \
-        --weight_decay 0.005 \
-        --layer_decay 0.75 \
-        --init_scale 0.5 \
         --test_num_segment 4 \
         --test_num_crop 3 \
         --epochs 90 \
@@ -72,20 +65,12 @@ torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         --batch_size 4 \
-        --input_size 224 \
-        --short_side_size 224 \
-        --save_ckpt_freq 10 \
-        --num_frames 16 \
-        --num_workers 10 \
+        --input_size ${INPUT_SIZE} \
         --sampling_rate 3 \
         --num_sample 2 \
         --num_workers 4 \
         --opt adamw \
         --num_segment 1 \
-        --lr 7e-5 \
-        --opt_betas 0.9 0.999 \
-        --weight_decay 0.05 \
-        --layer_decay 0.75 \
         --test_num_segment 10 \
         --test_num_crop 3 \
         --epochs 59 \
@@ -110,21 +95,12 @@ torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         --batch_size 4 \
-        --input_size 224 \
-        --short_side_size 224 \
-        --save_ckpt_freq 10 \
-        --num_frames 16 \
-        --num_workers 10 \
+        --input_size ${INPUT_SIZE} \
         --sampling_rate 4 \
         --num_sample 2 \
         --num_workers 4 \
         --opt adamw \
         --num_segment 1 \
-        --lr 7e-4 \
-        --min_lr 1e-8 \
-        --opt_betas 0.9 0.999 \
-        --weight_decay 0.05 \
-        --layer_decay 0.75 \
         --test_num_segment 5 \
         --test_num_crop 3 \
         --epochs 69 \
@@ -150,20 +126,12 @@ torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         --batch_size 4 \
-        --input_size 224 \
-        --short_side_size 224 \
-        --save_ckpt_freq 10 \
-        --num_frames 16 \
-        --num_workers 10 \
+        --input_size ${INPUT_SIZE} \
         --sampling_rate 3 \
         --num_sample 2 \
         --num_workers 4 \
         --opt adamw \
         --num_segment 1 \
-        --lr 7e-5 \
-        --opt_betas 0.9 0.999 \
-        --weight_decay 0.05 \
-        --layer_decay 0.75 \
         --test_num_segment 5 \
         --test_num_crop 3 \
         --epochs 59 \
@@ -189,20 +157,12 @@ torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1  --nproc_per
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         --batch_size 4 \
-        --input_size 224 \
-        --short_side_size 224 \
-        --save_ckpt_freq 10 \
-        --num_frames 16 \
-        --num_workers 10 \
+        --input_size ${INPUT_SIZE} \
         --sampling_rate 3 \
         --num_sample 2 \
         --num_workers 4 \
         --opt adamw \
         --num_segment 1 \
-        --lr 7e-5 \
-        --opt_betas 0.9 0.999 \
-        --weight_decay 0.05 \
-        --layer_decay 0.75 \
         --test_num_segment 5 \
         --test_num_crop 3 \
         --epochs 59 \
